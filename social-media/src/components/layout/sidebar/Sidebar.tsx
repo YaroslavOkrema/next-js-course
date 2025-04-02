602573c4f0a8668942012921e74b94e7e2e03c21
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-04-02 23:59:01 +0300

    feat: practice

--- social-media/src/components/layout/sidebar/Sidebar.tsx
+++ social-media/src/components/layout/sidebar/Sidebar.tsx
@@ -1,25 +1,26 @@
 import Image from 'next/image';
 import styles from './Sidebar.module.scss'
 import Link from "next/link";
-import { MessagesSquare, Phone, Settings, Sun, User2 } from "lucide-react";
 
 export function Sidebar() {
-    return <div className={styles.sidebar}>
-        <Image src='/logo.svg' alt='Logo' width={100} height={100}/>
-        <div>
-            <Link href='/friends'>
-                <User2 />
-            </Link>
-            <Link href='/call'>
-                <Phone />
-            </Link>
-            <Link href='/chats'>
-                <MessagesSquare />
-            </Link>
-            <Link href='/settings'>
-                <Settings />
-            </Link>
-        </div>
-        <Sun />
-    </div>
+    return (
+        <aside className={styles.sidebar}>
+            <Image src='/logo.svg' alt='Logo' priority width={50} height={50}/>
+            <div>
+                <Link href='/friends'>
+                    Friends
+                </Link>
+                <Link href='/call'>
+                    Call
+                </Link>
+                <Link href='/chats'>
+                    Chats
+                </Link>
+                <Link href='/settings'>
+                    Settings
+                </Link>
+            </div>
+        </aside>
+    )
+
 }
